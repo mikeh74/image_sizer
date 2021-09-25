4f1d04fb11149a448954f9651940faf226562521
Mike Horrocks <michael.horrocks@hotmail.co.uk>
2021-09-25 08:50:07 +0100

    Converted to single function and added main

--- fixer.py
+++ fixer.py
@@ -1,23 +1,29 @@
 from PIL import Image
 
-# read the image
-img = Image.open("images/IMG_1316.jpg")
 
-size = img.size
+def image_resizer():
+    # read the image
+    img = Image.open("images/IMG_1316.jpg")
 
-# reduce size by half
-size = ((size[0] // 2), (size[1] // 2))
+    size = img.size
 
-# resize image
-img = img.resize(size)
+    # reduce size by half
+    size = ((size[0] // 2), (size[1] // 2))
 
-# save resized image
-img.save('resize-output.jpg', quality=85)
+    # resize image
+    img = img.resize(size)
 
-# Create square version
-start_x = (size[0] - size[1]) // 2
+    # save resized image
+    img.save('resize-output.jpg', quality=85)
 
-box = (0, 0, size[1], size[1])
-size = (size[1], size[1])
-img = img.resize(size, box=box)
-img.save('resize-thumb.jpg', quality=85)
+    # Create square version
+    # start_x = (size[0] - size[1]) // 2
+
+    box = (0, 0, size[1], size[1])
+    size = (size[1], size[1])
+    img = img.resize(size, box=box)
+    img.save('resize-thumb.jpg', quality=85)
+
+
+if __name__ == "__main__":
+    image_resizer()
